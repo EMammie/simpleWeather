eb11d37d31a60f871c0ca76ecf8df676f9641344
emammie <Eugene@air.home>
2014-03-27 23:38:17 -0400

    Initial Commit

---
+++ simpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  simpleWeather
+//
+//  Created by Eugene Mammie on 3/27/14.
+//  Copyright (c) 2014 Eugene Mammie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
